180456e0a40bcd8b8855ac4cec4565237efdf9cf
shubh <shubha07m@gmail.com>
2023-08-01 12:16:19 -0700

    deleting unused files for restructuring

--- bitcounter.py
+++
@@ -1,9 +0,0 @@
-def countTotalBits(sem_string):
-    count = 0
-    for s in range(len(sem_string)):
-        count += 8
-    return count
-
-
-if __name__ == "__main__":
-    countTotalBits(sem_string='None')
