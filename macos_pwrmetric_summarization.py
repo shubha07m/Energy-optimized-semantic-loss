057e0a88a4ded40ab8b66498ca7fa66eb56d288d
shubh <shubha07m@gmail.com>
2023-10-06 13:00:44 -0700

    making major changes to accomodate raw powermetric output format change

--- macos_pwrmetric_summarization.py
+++ macos_pwrmetric_summarization.py
@@ -7,54 +7,40 @@ import pandas as pd
 import numpy as np
 import math
 
-# run this command in terminal, keeping any other applications than the intended one closed
-# sudo powermetrics -i 1000 --samplers cpu_power,gpu_power -a --hide-cpu-duty-cycle --show-usage-summary --show-extra-power-info | grep -E -- 'Power|Sampled' >> powersample.txt
-# Run the transformer resource comparison script to get timestamps
-# Once testing completed, delete the summary section from the powersample.txt log
-# TO-DO
-# Then run the current script after following above steps only
+# run this command in terminal, keeping any other applications than the intended one closed sudo powermetrics -i 1000
+# --samplers cpu_power,gpu_power -a --hide-cpu-duty-cycle --show-usage-summary --show-extra-power-info | grep -E --
+# 'Power|Sampled' >> powersample_encoder.txt Run the transformer resource comparison script to get timestamps Once
+# testing completed, Then run the current script after following these steps only
 
-data = pd.read_table('powersample.txt')
+data = pd.read_table('powersample_encoder.txt', header=None)
 
 Time_ = []
 CPU_Power = []
 GPU_Power = []
 Combined_Power = []
-System_instructions_retired = []
-System_instructions_per_clock = []
-
 j = 0
-for i in data.values:
 
-    if 'Sampled' in i[0]:
-        Time_.append(int(datetime.datetime.strptime(i[0][33:53], "%b %d %H:%M:%S %Y").timestamp()))
+for i in data.values:
+    if 'Sampled system activity' in i[0]:
+        Time_.append(int(datetime.datetime.strptime(i[0].split("(")[1].split(")")[0],
+                                                    "%a %b %d %H:%M:%S %Y %z").timestamp()))
 
     if 'CPU Power' in i[0]:
-        if float(i[0][11:-3]) == 0.0:
-            CPU_Power.append(0.0)
-        else:
-            CPU_Power.append(10 * math.log10(float(i[0][11:-3])))
+        if i[0][11:-3] != '':
+            CPU_Power.append(float(i[0][11:-3])/1000)
 
     if 'GPU Power' in i[0]:
         j += 1
         if j % 2:
-            if float(i[0][11:-3]) == 0.0:
-                GPU_Power.append(0.0)
-            else:
-                GPU_Power.append(10 * math.log10(float(i[0][11:-3])))
+            if i[0][11:-3] != '':
+                GPU_Power.append(float(i[0][11:-3])/1000)
 
     if 'Combined Power' in i[0]:
-        Combined_Power.append(10 * math.log10(float(i[0][34:-3])))
-
-    if 'System instructions retired' in i[0]:
-        System_instructions_retired.append(np.log10(float(i[0][29:])))
-
-    if 'System instructions per clock' in i[0]:
-        System_instructions_per_clock.append(i[0][31:])
+        if i[0][34:-3] != '':
+            Combined_Power.append(float(i[0][34:-3])/1000)
 
 df = pd.DataFrame({'Time': Time_, 'CPU_Power': CPU_Power, 'GPU_Power': GPU_Power,
-                   'Combined Power': Combined_Power,
-                   'System_instructions_retired': System_instructions_retired,
-                   'System_instructions_per_clock': System_instructions_per_clock})
+                   'Combined Power': Combined_Power})
 
-df.to_csv('powermetric.csv', index=False)
+# save as _decoder if using it for decoder side
+df.to_csv('resource_plotdata_encoder.csv', index=False)
